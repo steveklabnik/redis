6bbccc938f0c2ed7a01126a41c33ebcb022b8045
Steve Klabnik <steve@steveklabnik.com>
2013-09-17 12:03:52 -0700

    Moved command stuff out into its own method.
    Yup.

--- src/example/main.rs
+++ src/example/main.rs
@@ -15,16 +15,33 @@ enum Reply {
     MultiBulk(~str),
 }
 
+fn send_command_to_redis(mut redis: net::tcp::TcpStream, command: &[u8]) -> ~str{
+    redis.write(command);
+
+    let buf: &mut [u8] = [0];
+    let mut response : ~[u8] = ~[];
+
+    while(buf[0] != 13) {
+        match redis.read(buf) {
+            Some(_i) => {
+                response.push(buf[0]);
+            }
+            None => println("Couldn't read off of the socket.")
+        }
+    }
+
+    str::from_utf8(response)
+}
+
 
 fn main() {
     let ip = Ipv4Addr(127, 0, 0, 1);
-    let mut socket = net::tcp::TcpStream::connect(SocketAddr { ip: ip, port: 6379 });
-    socket.write(bytes!("*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"));
-    let buf: & mut [u8] = [0];
-    while(buf[0] != 13) {
-        match socket.read(buf) {
-            Some(i) => println(str::from_utf8(buf)),
-            None => println("NOPE")
-        }
+    let socket = net::tcp::TcpStream::connect(SocketAddr { ip: ip, port: 6379 });
+    match socket {
+        Some(redis) => {
+            let response = send_command_to_redis(redis, bytes!("*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"));
+            println(response);
+        },
+        None => println("Couldn't connect to a socket")
     }
 }
